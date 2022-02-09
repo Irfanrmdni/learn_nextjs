099fb947ae8dafe6463101180a40c4b96a718736
irfanrmdni <irfanrmdni209@gmail.com>
2022-02-09 20:01:34 +0700

    menambahkan catatan

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  // ? cara 1 JSON
+  // res.status(200).json({ name: 'John Doe' })
+
+  // ? cara 2 JSON
+  // res.status(200);
+  // res.setHeader('Content-Type', 'application/json');
+  // res.end(JSON.stringify({ name: 'irfan ramdani' }));
+
+  // ? 3 STRING
+  res.status(200)
+  res.end('irfan ramdani')
 }
--- pages/api/post.js
+++ pages/api/post.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
     res.status(200);
+    // res.end('hallo api')
     res.json({
         message: 'ini testing post'
     });
